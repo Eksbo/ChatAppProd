2307b5777bfb426bdd6a645962b015037caf423b
Valentin <valaimg2218@gmail.com>
2023-05-03 01:34:45 +0300

    updated chat room(desc, name, file update self messages)

--- src/components/core/Chat/Chat.jsx
+++ src/components/core/Chat/Chat.jsx
@@ -1,17 +1,28 @@
-import React, {useEffect, useRef, useState} from 'react';
-import {useSelector} from "react-redux";
+import React, { useEffect, useRef, useState } from 'react';
+import { useSelector } from "react-redux";
 
 import io from 'socket.io-client';
 
 import Message from "../../particles/Message/Message";
 
-import "./Chat.scss";
+import {File} from "../../../icon/File";
+import {Close} from "../../../icon/Close";
 
-export const Chat = ({id, idRoom}) => {
+import "./Chat.scss";
+import {api} from "../../../store/api";
+
+const FILE_STRING = "Attach file"
+
+export const Chat = ({ id, idRoom }) => {
     const [messages, setMessages] = useState([]);
+    const [fileName, setFileName] = useState(FILE_STRING);
+    const [roomName, setRoomName] = useState("Loading room name...");
+    const [roomDescription, setRoomDescription] = useState("");
+
 
     const bodyRef = useRef();
     const textField = useRef();
+    const fileField = useRef();
 
     const user = useSelector(state => state.users.editUser);
 
@@ -36,21 +47,54 @@ export const Chat = ({id, idRoom}) => {
 
     const joinRooms = async (rooms) => {
         await getRoomMessages();
-        await new Promise((resolve) => socket.emit('join-rooms', {rooms}, resolve));
+        await new Promise((resolve) => socket.emit('join-rooms', { rooms }, resolve));
     };
 
     const leaveRooms = async (rooms) => {
-        await new Promise((resolve) => socket.emit('leave-rooms', {rooms}, resolve));
+        await new Promise((resolve) => socket.emit('leave-rooms', { rooms }, resolve));
     };
 
-    const sendMessage = async (author, message, room) => {
+    const sendMessage = async (author, message, room, file) => {
         await new Promise((resolve) =>
-            socket.emit("send-message", { author, message, room }, resolve)
+            socket.emit("send-message", { author, message: message || '', room, filePath: file }, resolve)
         );
+
     };
+
+    const uploadFile = async ({ file }) => {
+        try {
+            const body = new FormData()
+            body.append('file', file)
+
+            const response = await fetch(`${process.env.REACT_APP_API_URL}/upload`, {
+                method: 'POST',
+                body,
+                headers: {
+                    Authorization: `Bearer ${token}`
+                }
+            })
+
+            if (!response.ok) throw response
+
+            const pathToFile = await response.json()
+
+            return pathToFile
+        } catch (e) {
+            throw e
+        }
+    }
+
 
     useEffect(() => {
         joinRooms([idRoom]);
+
+        api.get(`/rooms/${idRoom}`)
+            .then((res) => {
+              const {data} = res;
+
+              setRoomName(data?.name);
+              setRoomDescription(data?.description);
+            })
 
         return () => {
             leaveRooms([idRoom]);
@@ -61,15 +105,17 @@ export const Chat = ({id, idRoom}) => {
 
     useEffect(() => {
         socket.on('new-message', (message) => {
-            setMessages(prevState => [...prevState, {_id: new Date().getTime(), isAdmin: false, author: {username: message.author.username, id: user._id}, createdAt: message.createdAt, message: message.message.message}])
+
+            console.log('message', message);
+            setMessages(prevState => [...prevState, { _id: new Date().getTime(), isAdmin: false, author: { username: message.author.username, id: user._id }, createdAt: message.createdAt, message: message.message.message, filePath: message.message.filePath }])
         });
 
         socket.on('user-joined', ({ message }) => {
-            setMessages(prevState => [...prevState, {_id: new Date().getTime(), isAdmin: true, message}])
+            setMessages(prevState => [...prevState, { _id: new Date().getTime(), isAdmin: true, message }])
         });
 
         socket.on('user-left', ({ message }) => {
-            setMessages(prevState => [...prevState, {_id: new Date().getTime(), isAdmin: true, message}])
+            setMessages(prevState => [...prevState, { _id: new Date().getTime(), isAdmin: true, message }])
         });
     }, [idRoom]);
 
@@ -86,15 +132,45 @@ export const Chat = ({id, idRoom}) => {
         const author = user.username;
         const message = textField.current.value;
 
-        textField.current.value = '';
+        const file = fileField.current.files[0];
 
-        await sendMessage(author, message, room, null);
+        let path = "";
+
+        if (message !== "") {
+            if (file) {
+                path = await uploadFile({ file });
+
+                resetFile();
+            }
+
+            textField.current.value = '';
+
+            await sendMessage(author, message, room, path);
+        }
     };
+
+    const setFile = (e) => {
+        setFileName(fileField.current.files[0] ? fileField.current.files[0].name : 'Attach file')
+    }
+
+    const resetFile = (e) => {
+      e.stopPropagation();
+
+      setFileName(FILE_STRING);
+
+      fileField.current.value = '';
+    }
 
     return (
         <section className="chat">
             <div className="chat__top">
+                <h2>
+                    {roomName}
+                </h2>
 
+                <p>
+                    {roomDescription}
+                </p>
             </div>
 
             <div
@@ -113,6 +189,7 @@ export const Chat = ({id, idRoom}) => {
                             <Message
                                 key={message._id}
                                 message={message.message}
+                                filePath={message.filePath}
                                 isSelf={message.author.id === user._id}
                                 author={message.author}
                                 createdAt={message.createdAt}
@@ -133,6 +210,22 @@ export const Chat = ({id, idRoom}) => {
                     autoComplete="off"
                 />
 
+                <div className="chat__file">
+                    <p
+                        className="chat__attach"
+                        onClick={() => fileField.current.click()}
+                    >
+                        {fileName === FILE_STRING
+                            ? <File/>
+                            : <Close onClick={resetFile}/>
+                        }
+
+                        <span>{fileName}</span>
+                    </p>
+
+                    <input ref={fileField} onChange={setFile} type="file" id="file-input" />
+                </div>
+
                 <div className="chat__controls">
                     <button
                         className="chat__button"
@@ -145,5 +238,3 @@ export const Chat = ({id, idRoom}) => {
         </section>
     );
 }
-
-
